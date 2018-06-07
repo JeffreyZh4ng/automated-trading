26552e2d271d9208be50104996156ee74dfb0ea6
Jeffrey Zhang <jeffrey.zh4ng@gmail.com>
2018-06-07 00:43:26 -0700

    Made the login method store the token locally so that we can use os variables to automate the pasting of the token.

--- AutomatedTrading/Login.py
+++ AutomatedTrading/Login.py
@@ -1,11 +1,10 @@
-import json
 import requests
 
 
-class TestClass:
+class Login:
 
     @staticmethod
-    def login(username, password):
+    def login_with_credentials(username, password):
         headers = {
             'Accept': 'application/json',
         }
@@ -13,6 +12,21 @@ class TestClass:
         data = [
             ('username', username),
             ('password', password),
+        ]
+
+        response = requests.post('https://api.robinhood.com/api-token-auth/', headers=headers, data=data)
+        return response
+
+    @staticmethod
+    def login_with_mfa(username, password, mfa_code):
+        headers = {
+            'Accept': 'application/json',
+        }
+
+        data = [
+            ('username', username),
+            ('password', password),
+            ('mfa_code', mfa_code),
         ]
 
         response = requests.post('https://api.robinhood.com/api-token-auth/', headers=headers, data=data)
--- Main.py
+++ Main.py
@@ -1,10 +1,36 @@
+import os
 import sys
-from AutomatedTrading.Login import TestClass
+import json
+from AutomatedTrading.Login import Login
 
 
 if __name__ == '__main__':
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    response = TestClass.login(username,password)
-    print(response.text)
+    if len(sys.argv) < 2:
+        print('You must specify a command EX: Login, Logout...')
+        sys.exit()
+
+    command_name = sys.argv[1]
+    command_name = command_name.casefold()
+
+    if command_name == 'login':
+        username = sys.argv[2]
+        password = sys.argv[3]
+
+        response = ''
+        if len(sys.argv) == 5:
+            mfa_code = sys.argv[4]
+            print('Entering the MFA %s' % mfa_code)
+            response = Login.login_with_mfa(username, password, mfa_code)
+
+        else:
+            print('Entering login credentials')
+            response = Login.login_with_credentials(username, password)
+
+        if response.status_code == 200:
+            json_data = json.loads(response.text)
+            token = json_data['token']
+            os.environ["ROBINHOOD_CURRENT_LOGIN_TOKEN"] = token
+
+        print(response.text)
+        sys.exit()
